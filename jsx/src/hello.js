231c48e00f4d78b89ffd9af6aaf835e989f0a77c
Your Name <[ashutosh.singh3889@gmail.com]>
2021-10-16 14:09:32 +0530

    my first project  commit

--- jsx/src/hello.js
+++ jsx/src/hello.js
@@ -4,7 +4,10 @@ class Hello extends React.Component{
     render(){
         return(
             <div>
-                HELLO WORLD
+                
+                <label className  = "label" htmlFor= "name">Enter Name:</label>
+                <input id = "name" type = "text"></input>
+                <button style = {{backgroundColor:'white'}}>Click me!</button>
             </div>
         );
     }
